5b38643d8cb00bf844b75b8f78d64700b48859e0
Scott Andrew <scott@nwdm.tech>
2021-06-16 20:09:43 -0700

    Fix linux build.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@
 import PackageDescription
 import Foundation
 
+#if os(Linux)
+let SDLCFlags=["-I/usr/include/SDL2"]
+#else
+let SDLCFlags=["-I/usr/local/Cellar/sdl2/2.0.14_1/include/SDL2", "-I/opt/homebrew/include/SDL2"]
+#endif
+
 let package = Package(
     name: "LVGLSwift",
     products: [
@@ -57,7 +63,7 @@ let package = Package(
             cSettings: [
                         .headerSearchPath("lv_drivers"),
                         .headerSearchPath("lvgl"),
-                        .unsafeFlags(["-I/usr/local/Cellar/sdl2/2.0.14_1/include/SDL2", "-I/opt/homebrew/include/SDL2", "-D THREAD_SAFE", "-D LV_LVGL_H_INCLUDE_SIMPLE"])
+                        .unsafeFlags(SDLCFlags + ["-D THREAD_SAFE", "-D LV_LVGL_H_INCLUDE_SIMPLE"])
                         ],
         linkerSettings: [.unsafeFlags(["-L/opt/homebrew/lib","-lSDL2"])]
             
